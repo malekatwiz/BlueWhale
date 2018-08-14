86a1aa9cb5e1f4c1ad13e9baed570fd2bb68d20f
Malek Atwiz <malekatwiz@gmail.com>
2018-08-14 10:38:44 -0400

    cleaning up

--- BlueWhale.Main/Controllers/HomeController.cs
+++ BlueWhale.Main/Controllers/HomeController.cs
@@ -2,25 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using BlueWhale.Main.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Configuration;
 
 namespace BlueWhale.Main.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-        private readonly IConfiguration _configuration;
-        public HomeController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
--- BlueWhale.Security/Models/UserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace BlueWhale.Security.Models
-{
-    public class UserRole : IdentityRole
-    {
-        public string Description { get; set; }
-    }
-}
