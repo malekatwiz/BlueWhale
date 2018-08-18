b7762bfc3f452ad13927d2517654ad82ce6527c1
Malek Atwiz <malekatwiz@gmail.com>
2018-08-18 00:15:42 -0400

    registration form

--- BlueWhale.Security/Controllers/AccountController.cs
+++ BlueWhale.Security/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using BlueWhale.Security.Models;
 using BlueWhale.Security.Services.LoginService;
 using IdentityServer4.Events;
 using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlueWhale.Security.Controllers
@@ -13,14 +14,17 @@ namespace BlueWhale.Security.Controllers
         private readonly IIdentityServerInteractionService _identityServerInteractionService;
         private readonly IEventService _eventService;
         private readonly ILoginService _loginService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         public AccountController(IIdentityServerInteractionService identityServerInteractionService,
             IEventService eventService,
-            ILoginService loginService)
+            ILoginService loginService,
+            UserManager<ApplicationUser> userManager)
         {
             _identityServerInteractionService = identityServerInteractionService;
             _eventService = eventService;
             _loginService = loginService;
+            _userManager = userManager;
         }
 
         [HttpGet, Route("login")]
@@ -60,6 +64,33 @@ namespace BlueWhale.Security.Controllers
 
                 await _eventService.RaiseAsync(new UserLoginFailureEvent(model.Username, "Invalid credentials"));
             }
+            return View(string.Empty);
+        }
+
+        [HttpGet, Route("register")]
+        public IActionResult Register(string returnUrl) => View();
+
+        [HttpPost, Route("register")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.User.UserName,
+                    Email =  model.Email,
+                    FirstName = model.User.FirstName,
+                    LastName = model.User.LastName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+
+                }
+            }
+
             return View(string.Empty);
         }
     }
---
+++ BlueWhale.Security/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlueWhale.Security.Models
+{
+    public class RegisterViewModel
+    {
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
